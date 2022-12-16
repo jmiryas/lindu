f16f52eab5c7908a23af0b71539f25343561ee25
Rizky Ramadhan <jmiryas@gmail.com>
2022-12-17 02:11:44 +0700

    first commit

---
+++ lib/contants/constants.dart
@@ -0,0 +1,12 @@
+// Task / Workmanager
+
+const gempaTerbaruTask = "gempaTerbaruAPI";
+
+// Shared preferences
+
+const prefsEarthquake = "currentEarthquake";
+
+// API
+
+const String gempaTerbaruAPI =
+    "https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json";
---
+++ lib/utils/utils.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import '../contants/constants.dart';
+import '../models/earthquake_model.dart';
+
+// Menampilkan informasi gempa melalui notifikasi
+
+void displayNotification(
+  String magnitude,
+  String description,
+) async {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  const AndroidNotificationDetails androidNotificationDetails =
+      AndroidNotificationDetails(
+    'Earthquake-1',
+    'Earthquake',
+    channelDescription: 'Latest earthquake',
+    importance: Importance.max,
+    priority: Priority.high,
+    ticker: 'ticker',
+    styleInformation: BigTextStyleInformation(""),
+  );
+
+  const NotificationDetails notificationDetails =
+      NotificationDetails(android: androidNotificationDetails);
+
+  await flutterLocalNotificationsPlugin.show(
+    0,
+    "Gempa Magnitude $magnitude",
+    description,
+    notificationDetails,
+    payload: gempaTerbaruTask,
+  );
+}
+
+// Mengecek apakah gempa yang berasal dari API
+// merupakan data baru atau tidak di shared preferences
+
+bool isNewEarthquake(
+    SharedPreferences prefs, EarthquakeModel fetchedEartquake) {
+  if (prefs.containsKey(prefsEarthquake)) {
+    var prefsDateTime = DateTime.parse(
+        jsonDecode(prefs.getString(prefsEarthquake)!)["DateTime"]);
+
+    var comparationResult = prefsDateTime.isBefore(fetchedEartquake.dateTime!);
+
+    return comparationResult;
+  }
+
+  return false;
+}
